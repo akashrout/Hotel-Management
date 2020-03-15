064555d3c8ca7a16f3b2b7b43c64e7083a5bacea
Akash <akashkumarrout7@gmail.com>
2020-03-15 23:03:00 +0530

    Return all updated values in update API

--- src/main/java/com/example/demo/hotel/food/category/rest/FoodCategoryRestController.java
+++ src/main/java/com/example/demo/hotel/food/category/rest/FoodCategoryRestController.java
@@ -2,12 +2,9 @@ package com.example.demo.hotel.food.category.rest;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +24,7 @@ import com.example.demo.hotel.food.category.validations.CategoryValidator;
  * The Class FoodCategoryRestController.
  */
 @RestController
-@RequestMapping("/hotel")
+@RequestMapping("/category")
 public class FoodCategoryRestController {
 
 	/** The category service. */
@@ -36,7 +33,7 @@ public class FoodCategoryRestController {
 	@Autowired
 	private CategoryValidator categoryValidator;
 
-	@GetMapping("allcategory")
+	@GetMapping("getall")
 	public ResponseEntity<List<FoodCategoryEntity>> listAllProducts() {
 		List<FoodCategoryEntity> foodCategories = categoryService.getAllCategory();
 		if (foodCategories.isEmpty()) {
@@ -51,8 +48,7 @@ public class FoodCategoryRestController {
 	 * @param foodCategory the food category
 	 * @return the status
 	 */
-	@PostMapping("addcategory")
-	// @Validated
+	@PostMapping("create")
 	public ResponseEntity<FoodCategoryBean> addCategory(@RequestBody FoodCategoryBean foodCategory) {
 		System.out.println("Inputed Data Are: " + foodCategory);
 
@@ -69,12 +65,12 @@ public class FoodCategoryRestController {
 			}
 
 		} else {
-			    return new ResponseEntity(new Status("Unable to create. Some fields are missing "), HttpStatus.CONFLICT);
+			return new ResponseEntity(new Status("Unable to create. Some fields are missing "), HttpStatus.CONFLICT);
 
 		}
 
 // 
-//		if ((foodCategory.getCategoryId()d() == null) || (foodCategory.getCategory_name() == null)
+//		if ((foodCategory.getCategoryId() == null) || (foodCategory.getCategory_name() == null)
 //				|| (foodCategory.getCategory_description() == null)) { 
 //
 ////			Status status = new Status("Please Provide all details");
@@ -101,24 +97,22 @@ public class FoodCategoryRestController {
 	 * @param foodCategory the food category
 	 * @return the status
 	 */
-	@PutMapping("updatecategory")
+	@PutMapping("update")
 	public ResponseEntity<FoodCategoryBean> updateCategory(@RequestBody FoodCategoryBean foodCategory) {
+		if (categoryValidator.validateCategoryForEmptyData(foodCategory)) {
 
-		if (categoryValidator.isIdPresent(foodCategory.getCategoryId())) {
-			if (categoryValidator.isCategoryNamePresent(foodCategory.getCategoryName())) {
-				return new ResponseEntity(new Status("Category Name already Present, Please try another one for update... "), HttpStatus.NOT_ACCEPTABLE);
-			}
-			else {
-				
-				categoryService.updateCategory(foodCategory);
-				return new ResponseEntity(new Status("Updated Succesfully... "), HttpStatus.ACCEPTED);
-				
-			}
+			if (categoryValidator.isIdPresent(foodCategory.getCategoryId())) {
+				FoodCategoryBean bean = categoryService.updateCategory(foodCategory);
+				return new ResponseEntity(bean, HttpStatus.ACCEPTED);
 
+			} else {
+
+				return new ResponseEntity(new Status("Unable to update, Category Id or Category Name not available "),
+						HttpStatus.NOT_ACCEPTABLE);
+
+			}
 		} else {
-
-			return new ResponseEntity(new Status("Unable to update, Category Id or Category Name not available "),
-					HttpStatus.CONFLICT);
+			return new ResponseEntity(new Status("Unable to create. Some fields are missing "), HttpStatus.CONFLICT);
 
 		}
 	}
@@ -129,7 +123,7 @@ public class FoodCategoryRestController {
 	 * @param foodCategory the food category
 	 * @return the status
 	 */
-	@DeleteMapping("deletecategory")
+	@DeleteMapping("delete")
 	public ResponseEntity<FoodCategoryBean> deleteCategory(@RequestBody FoodCategoryBean foodCategory) {
 		if (categoryService.iscategorypresent(foodCategory.getCategoryId())) {
 			categoryService.deleteCategory(foodCategory.getCategoryId());
@@ -138,7 +132,8 @@ public class FoodCategoryRestController {
 		} else {
 			Status status = new Status("Category Not Availabale");
 
-			return new ResponseEntity(new Status("This Category Id Is not available, Unable to delete... "), HttpStatus.CONFLICT);
+			return new ResponseEntity(new Status("This Category Id Is not available, Unable to delete... "),
+					HttpStatus.CONFLICT);
 
 		}
 	}
